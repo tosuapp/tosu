b4c476fbce246f1a1474a561c323e2a1ea7d83b4
ck <21735205+cyperdark@users.noreply.github.com>
2024-02-13 16:20:01 +0300

    fix: memory leak + tournament client

--- packages/find-process/src/lib.rs
+++ packages/find-process/src/lib.rs
@@ -4,21 +4,27 @@ use sysinfo::System;
 fn process_by_name(mut cx: FunctionContext) -> JsResult<JsValue> {
     let sys = System::new_all();
     let process_name = cx.argument::<JsString>(0)?.value(&mut cx);
-    let obj = cx.empty_object();
+    let result_array = JsArray::new(&mut cx, 0);
+
+    let mut index = 0;
 
     for process in sys.processes_by_name(&process_name) {
-        let id = cx.number(process.pid().as_u32());
-        let name = cx.string(process.name().to_string());
-        let cmd = cx.string(process.cmd()[0].to_string());
+        if process.name() == &process_name {
+            let id = cx.number(process.pid().as_u32());
+            let name = cx.string(process.name().to_string());
+            let cmd = cx.string(process.cmd()[0].to_string());
 
-        let _ = obj.set(&mut cx, "pid", id);
-        let _ = obj.set(&mut cx, "name", name);
-        let _ = obj.set(&mut cx, "cmd", cmd);
+            let obj = cx.empty_object();
+            let _ = obj.set(&mut cx, "pid", id);
+            let _ = obj.set(&mut cx, "name", name);
+            let _ = obj.set(&mut cx, "cmd", cmd);
 
-        return Ok(obj.upcast());
+            let _ = result_array.set(&mut cx, index, obj);
+            index += 1;
+        }
     }
 
-    Ok(cx.null().upcast())
+    Ok(result_array.upcast())
 }
 
 #[neon::main]
