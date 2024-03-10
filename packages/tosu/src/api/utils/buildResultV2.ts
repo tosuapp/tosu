5daec8d07bedc4fed849f8037496fa1d45ff1e49
ck <21735205+cyperdark@users.noreply.github.com>
2024-03-10 09:20:09 +0300

    feat: Moved `v2/keys` to `v2/precise`

--- packages/tosu/src/api/utils/buildResultV2.ts
+++ packages/tosu/src/api/utils/buildResultV2.ts
@@ -459,22 +459,25 @@ export const buildPreciseResult = (service: DataRepo): PreciseApiAnswer => {
     const { gamePlayData } = service.getServices(['gamePlayData']);
 
     return {
-        k1: {
-            isPressed: gamePlayData.KeyOverlay.K1Pressed,
-            count: gamePlayData.KeyOverlay.K1Count
+        keys: {
+            k1: {
+                isPressed: gamePlayData.KeyOverlay.K1Pressed,
+                count: gamePlayData.KeyOverlay.K1Count
+            },
+            k2: {
+                isPressed: gamePlayData.KeyOverlay.K2Pressed,
+                count: gamePlayData.KeyOverlay.K2Count
+            },
+            m1: {
+                isPressed: gamePlayData.KeyOverlay.M1Pressed,
+                count: gamePlayData.KeyOverlay.M1Count
+            },
+            m2: {
+                isPressed: gamePlayData.KeyOverlay.M2Pressed,
+                count: gamePlayData.KeyOverlay.M2Count
+            }
         },
-        k2: {
-            isPressed: gamePlayData.KeyOverlay.K2Pressed,
-            count: gamePlayData.KeyOverlay.K2Count
-        },
-        m1: {
-            isPressed: gamePlayData.KeyOverlay.M1Pressed,
-            count: gamePlayData.KeyOverlay.M1Count
-        },
-        m2: {
-            isPressed: gamePlayData.KeyOverlay.M2Pressed,
-            count: gamePlayData.KeyOverlay.M2Count
-        }
+        hitErrors: gamePlayData.HitErrors
     };
 };
 
