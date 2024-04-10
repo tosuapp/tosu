df19be701cbbb360e11ff589d5debd595f739f31
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-11 01:40:47 +0300

    fix: Improve performance for precise data

--- packages/tosu/src/entities/GamePlayData/index.ts
+++ packages/tosu/src/entities/GamePlayData/index.ts
@@ -410,20 +410,16 @@ export class GamePlayData extends AbstractEntity {
 
             const leaderStart = patterns.getLeaderStart();
 
-            const errors: Array<number> = [];
-
             const base = process.readInt(this.scoreBase + 0x38);
             const items = process.readInt(base + 0x4);
             const size = process.readInt(base + 0xc);
 
-            for (let i = 0; i < size; i++) {
+            for (let i = this.HitErrors.length - 1; i < size; i++) {
                 const current = items + leaderStart + 0x4 * i;
                 const error = process.readInt(current);
 
-                errors.push(error);
+                this.HitErrors.push(error);
             }
-
-            this.HitErrors = errors;
         } catch (exc) {
             wLogger.error('GD(updateHitErrors) failed to parse hitErrors');
             wLogger.debug(exc);
