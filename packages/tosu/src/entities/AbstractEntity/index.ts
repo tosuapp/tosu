ad484dfe099dec4e450a642848c36eb73e04f3d2
ck <21735205+cyperdark@users.noreply.github.com>
2024-05-09 17:23:30 +0300

    feat: Functions print out errors when they repeat more than `x` time

--- packages/tosu/src/entities/AbstractEntity/index.ts
+++ packages/tosu/src/entities/AbstractEntity/index.ts
@@ -1,6 +1,9 @@
+import { wLogger } from '@tosu/common';
+
 import { OsuInstance } from '@/objects/instanceManager/osuInstance';
 
 export abstract class AbstractEntity {
+    errorsCount: { [key: string | number]: number } = {};
     osuInstance: OsuInstance;
 
     constructor(osuInstance: OsuInstance) {
@@ -9,5 +12,20 @@ export abstract class AbstractEntity {
 
     updateState() {
         throw Error('Error: updateState not implemented');
+    }
+
+    reportError(id: string | number, maxAmount: number, ...args: any[]) {
+        this.errorsCount[id] = (this.errorsCount[id] || 0) + 1;
+
+        if (this.errorsCount[id] <= maxAmount) {
+            wLogger.debugError(...args);
+            return;
+        }
+
+        wLogger.error(...args);
+    }
+
+    resetReportCount(id: string | number) {
+        this.errorsCount[id] = 0;
     }
 }
