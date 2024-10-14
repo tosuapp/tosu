2e9bf29e5f9bf8f066836fcaee497337675a8f6f
Cherry <aazetthr@gmail.com>
2024-10-15 00:07:51 +0300

    feat: add lazer placeholder

---
+++ packages/tosu/src/instances/lazerInstance.ts
@@ -0,0 +1,31 @@
+import { LazerMemory } from '@/memory/lazer';
+import { Gameplay } from '@/states/gameplay';
+import { Global } from '@/states/global';
+
+import { AbstractInstance } from '.';
+
+export class LazerInstance extends AbstractInstance {
+    memory: LazerMemory;
+
+    constructor(pid: number) {
+        super(pid);
+
+        this.memory = new LazerMemory(this.process, this);
+    }
+
+    start(): void | Promise<void> {
+        super.start();
+
+        this.initiateDataLoops();
+        this.watchProcessHealth();
+    }
+
+    injectGameOverlay(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    regularDataLoop(): void {}
+    preciseDataLoop(global: Global, gameplay: Gameplay): void {
+        throw new Error('Method not implemented.' + global + gameplay);
+    }
+}
