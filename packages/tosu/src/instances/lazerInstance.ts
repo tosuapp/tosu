161f4f6778e95229bf8f604eccf0e61ebaad8f3c
ck <21735205+cyperdark@users.noreply.github.com>
2024-10-18 12:34:22 +0300

    chore: fix behaviour when it doesnt resolve patterns but starts dataLoops anyways

--- packages/tosu/src/instances/lazerInstance.ts
+++ packages/tosu/src/instances/lazerInstance.ts
@@ -15,13 +15,6 @@ export class LazerInstance extends AbstractInstance {
         this.memory = new LazerMemory(this.process, this);
     }
 
-    start(): void | Promise<void> {
-        super.start();
-
-        this.initiateDataLoops();
-        this.watchProcessHealth();
-    }
-
     injectGameOverlay(): void {
         throw new Error('Method not implemented.');
     }
