99a226b50422b6d38e0ba5f7e8d473b2cabce986
ck <21735205+cyperdark@users.noreply.github.com>
2024-12-09 15:53:20 +0300

    fix: Do not create empty folder before extracting & send actual error

--- packages/common/utils/unzip.ts
+++ packages/common/utils/unzip.ts
@@ -5,15 +5,12 @@ import { wLogger } from './logger';
 
 export const unzip = (zipPath: string, extractPath: string): Promise<string> =>
     new Promise((resolve, reject) => {
-        if (!fs.existsSync(extractPath)) {
-            fs.mkdirSync(extractPath);
-        }
-
         const zip = new AdmZip(zipPath);
 
         try {
-            // Extract all contents of the zip file to the specified destination
-            zip.extractAllTo(extractPath, /* overwrite */ true);
+            if (!fs.existsSync(extractPath)) fs.mkdirSync(extractPath);
+
+            zip.extractAllTo(extractPath, true);
             resolve(extractPath);
         } catch (error) {
             wLogger.error('[unzip]', (error as any).message);
