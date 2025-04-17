4d144cdc10800115ff8ed9bf32f79af8c6bb014d
GabuTheDev <107962621+GabuTheDev@users.noreply.github.com>
2025-04-17 18:34:11 +0300

    feat: Disallow tosu startup under OneDrive directories.

--- packages/tosu/src/index.ts
+++ packages/tosu/src/index.ts
@@ -1,6 +1,8 @@
 import { argumetsParser, config, wLogger, watchConfigFile } from '@tosu/common';
 import { Server } from '@tosu/server';
 import { autoUpdater, checkUpdates } from '@tosu/updater';
+import { homedir } from 'node:os';
+import { resolve } from 'path';
 import { Process } from 'tsprocess/dist/process';
 
 import { InstanceManager } from '@/instances/manager';
@@ -37,9 +39,34 @@ const currentVersion = require(process.cwd() + '/_version.js');
         }
     }
 
-    wLogger.info('Searching for osu!');
+    let isInOneDrive = false;
+    if (process.platform === 'win32') {
+        const currentPath = resolve().toLowerCase();
+        const homeDir = homedir().toLowerCase();
 
-    httpServer.start();
-    instanceManager.runWatcher();
-    instanceManager.runDetemination();
+        // NOTE: The listed strings are only the default OneDrive paths.
+        const oneDrivePaths = [
+            'OneDrive',
+            'OneDrive - Personal',
+            'OneDrive - Business',
+            'OneDrive for Business'
+        ].map((path) => resolve(homeDir, path).toLowerCase());
+
+        isInOneDrive = oneDrivePaths.some((path) =>
+            currentPath.startsWith(path)
+        );
+    }
+
+    if (isInOneDrive === false) {
+        wLogger.info('Searching for osu!');
+
+        httpServer.start();
+        instanceManager.runWatcher();
+        instanceManager.runDetemination();
+    } else {
+        wLogger.warn(
+            'tosu cannot run from a OneDrive folder due to potential sync conflicts and performance issues.'
+        );
+        wLogger.warn('Please move the application to a local folder');
+    }
 })();
