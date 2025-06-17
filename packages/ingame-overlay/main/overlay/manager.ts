87f9996ed0e62b2a7e154264644f1fc94ce69e3e
storycraft <storycraft@pancake.sh>
2025-06-17 15:31:30 +0900

    chore: remove wql-process-monitor, osu process detection

--- packages/ingame-overlay/main/overlay/manager.ts
+++ packages/ingame-overlay/main/overlay/manager.ts
@@ -1,21 +1,10 @@
-import { promises as wql } from '@jellybrick/wql-process-monitor';
-import EventEmitter from 'node:events';
-import { Process } from 'tsprocess';
-
 import { OverlayProcess } from './process';
 
-type ManagerEventEmitter = EventEmitter<{
-    added: [pid: number, overlay: OverlayProcess];
-    removed: [pid: number];
-}>;
-
 export class OverlayManager {
-    readonly event: ManagerEventEmitter = new EventEmitter();
-
     private readonly map: Map<number, OverlayProcess> = new Map();
-    private readonly abortController: AbortController = new AbortController();
 
-    private async addOverlay(pid: number) {
+    // Run overlay in provided process
+    async runOverlay(pid: number) {
         try {
             console.debug('initializing ingame overlay pid:', pid);
             const overlay = await OverlayProcess.initialize(pid);
@@ -28,44 +17,15 @@ export class OverlayManager {
             }
 
             this.map.set(pid, overlay);
-            this.event.emit('added', pid, overlay);
-            overlay.event.once('destroyed', () => {
-                this.map.delete(pid);
-                this.event.emit('removed', pid);
-            });
+            return new Promise<void>((resolve) =>
+                overlay.event.once('destroyed', () => {
+                    this.map.delete(pid);
+                    resolve();
+                })
+            );
         } catch (e) {
             console.warn('overlay injection failed err:', e);
         }
-    }
-
-    async run() {
-        const signal = this.abortController.signal;
-
-        const emitter = await wql.subscribe({
-            creation: true
-        });
-        emitter.on('creation', ([name, pid]) => {
-            if (name === 'osu!.exe' || name === 'osulazer.exe') {
-                const id = Number.parseInt(pid);
-                if (isNaN(id)) {
-                    return;
-                }
-
-                this.addOverlay(id);
-            }
-        });
-
-        const osuProcesses = Process.findProcesses([
-            'osu!.exe',
-            'osulazer.exe'
-        ]);
-        for (const osuGamePid of osuProcesses) {
-            await this.addOverlay(osuGamePid);
-        }
-
-        await new Promise((resolve) => {
-            signal.addEventListener('abort', resolve, { once: true });
-        });
     }
 
     reloadAll() {
@@ -75,9 +35,8 @@ export class OverlayManager {
     }
 
     destroy() {
-        this.abortController.abort();
+        for (const overlay of this.map.values()) {
+            overlay.destroy();
+        }
     }
 }
-
-// fix wql
-wql.createEventSink();
