8a902d2baa9eba03b0991d3d4a15b13c8d55732f
GabuTheDev <107962621+GabuTheDev@users.noreply.github.com>
2026-01-29 15:25:19 +0200

    chore: Improve colors and switch to ANSI RGB.

--- packages/common/enums/logging.ts
+++ packages/common/enums/logging.ts
@@ -1,13 +1,13 @@
 export const LogColor = {
-    Info: '\x1b[102m\x1b[30m',
-    Error: '\x1b[41m\x1b[37m',
-    Debug: '\x1b[100m\x1b[37m',
-    Time: '\x1b[100m\x1b[37m',
-    DebugError: '\x1b[45m\x1b[37m',
-    Warn: '\x1b[43m\x1b[30m',
-    Reset: '\x1b[0m',
-    Grey: '\x1b[90m',
-    GreenText: '\x1b[32m'
+    Info: '\x1b[38;2;80;250;123m',
+    Warn: '\x1b[38;2;255;184;108m',
+    Error: '\x1b[38;2;255;85;85m',
+    DebugError: '\x1b[38;2;189;147;249m',
+    Debug: '\x1b[38;2;120;120;120m',
+    Time: '\x1b[38;2;80;80;80m',
+    Grey: '\x1b[38;2;170;170;170m',
+    GreenText: '\x1b[38;2;60;200;100m',
+    Reset: '\x1b[0m'
 };
 
 export const LogSymbol = {
